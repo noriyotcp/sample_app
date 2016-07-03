6d135cf71741186002b2102c59f1ccae38619035
Noriyo Akita <noriyo.akita@gmail.com>
2016-07-03 14:20:03 +0000

    (users_edit_test) Add test for successful edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,7 +5,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "unsuccessful edit" do
+  test 'unsuccessful edit' do
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: '',
@@ -13,5 +13,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                 password: 'foo',
                                 password_confirmation: 'bar' } }
     assert_template 'users/edit'
+  end
+
+  test 'successful edit' do
+    get edit_user_path(@user)
+    name = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: '',
+                                              password_confirmation: '' } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
   end
 end
