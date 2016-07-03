26eb539995dd647866348e57c53132c4fafd9b3a
Noriyo Akita <noriyo.akita@gmail.com>
2016-07-03 14:32:32 +0000

    (users_controller) Display a flash message and redirect to login page when non-logged-in user accesses to edit or update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
   end
@@ -39,5 +40,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
   end
 end
