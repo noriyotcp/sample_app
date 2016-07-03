1ca5421eecca47377128c484f24c8cdd3760582d
Noriyo Akita <noriyo.akita@gmail.com>
2016-07-03 14:50:43 +0000

    (users_controller) Add before_action :correct_user
    redirect to root when the other user tries to get another user's edit or update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
   end
@@ -47,5 +48,10 @@ class UsersController < ApplicationController
       flash[:danger] = 'Please log in.'
       redirect_to login_url
     end
+  end
+
+  def correct_user
+    set_user
+    redirect_to root_url unless @user == current_user
   end
 end
