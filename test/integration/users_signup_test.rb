b2bf124da76b3b67eda30a2b8280463e2145ad69
Noriyo Akita <noriyo.akita@gmail.com>
2016-07-04 07:44:52 +0000

    (users_signup_test) Add tests for activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test 'invalid singup information' do
     get signup_path
     assert_no_difference 'User.count' do
@@ -22,10 +26,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                 password: 'foobar',
                                 password_confirmation: 'foobar' }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
+    # Try to log in before activation.
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    # Invalid activation token
+    get edit_account_activation_path('invalid', email: user.email)
+    assert_not is_logged_in?
+
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
-    # assert_template 'users/show'
-    # assert is_logged_in?
+    assert_template 'users/show'
+    assert is_logged_in?
     assert_not flash.empty?
-    assert_equal flash[:info], 'Please check your email to activate your account.'
+    assert_equal flash[:success], 'Account activated!'
   end
 end
