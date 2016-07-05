79307d9b1f4f41ee55cbc5a9cb6f6141e03716bd
Noriyo Akita <noriyo.akita@gmail.com>
2016-07-05 08:27:20 +0000

    Fix rubocop

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -6,7 +6,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "password resets" do
+  test 'password resets' do
     get new_password_reset_path
     assert_template 'password_resets/new'
 
@@ -43,25 +43,24 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     # Right email, right token
     get edit_password_reset_path(user.reset_token, email: user.email)
     assert_template 'password_resets/edit'
-    assert_select "input[name=email][type=hidden][value=?]", user.email
+    assert_select 'input[name=email][type=hidden][value=?]', user.email
 
     # Invalid password & confirmation
     patch password_reset_path(user.reset_token), params: { email: user.email,
                                                            user: { password: 'foobar',
-                                                           password_confirmation: 'barquux' } }
+                                                                   password_confirmation: 'barquux' } }
     assert_select 'div#error_explanation'
-
 
     # Empty password
     patch password_reset_path(user.reset_token), params: { email: user.email,
                                                            user: { password: '',
-                                                           password_confirmation: '' } }
+                                                                   password_confirmation: '' } }
     assert_select 'div#error_explanation'
 
     # Valid password & confirmation
     patch password_reset_path(user.reset_token), params: { email: user.email,
                                                            user: { password: 'foobaz',
-                                                           password_confirmation: 'foobaz' } }
+                                                                   password_confirmation: 'foobaz' } }
     assert is_logged_in?
     assert_nil user.reload.reset_digest
     assert_not flash.empty?
@@ -75,8 +74,8 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     @user = assigns(:user)
     @user.update_attribute(:reset_sent_at, 3.hours.ago)
     patch password_reset_path(@user.reset_token), params: { email: @user.email,
-                                                           user: { password: 'foobaz',
-                                                           password_confirmation: 'foobaz' } }
+                                                            user: { password: 'foobaz',
+                                                                    password_confirmation: 'foobaz' } }
     assert_response :redirect
     follow_redirect!
     assert_match /expired/i, response.body
