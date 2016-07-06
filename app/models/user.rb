6a32d936f4c730e302a6462ad4eaea926a735331
Noriyo Akita <noriyo.akita@gmail.com>
2016-07-06 07:37:37 +0000

    (models/user) Add has_many :active_relationships and :following
    active_relationships has Relationship class and follower_id as foreign_key, user has many following (followed)

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   class << self
     def digest(string)
