055e2983e633f11e902266f2f342e998f612a319
Noriyo Akita <noriyo.akita@gmail.com>
2016-07-06 07:58:36 +0000

    (seeds) Create following users and followers of the first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followed = users[3..40]
+following.each { |followed| user.follow(followed) }
+followed.each { |follower| follower.follow(user) }
