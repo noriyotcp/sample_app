7a3076c275d61e9036f1177f5427f672d677cb6a
Noriyo Akita <noriyo.akita@gmail.com>
2016-07-06 07:13:36 +0000

    (relationship_test) Add tests for relationship
    get relationship empty to pass tests

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @relationship = Relationship.new(follower_id: users(:michael).id,
+                                     followed_id: users(:archer).id)
+  end
+
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  test "should require a follower" do
+    @relationship.follower = nil
+    assert_not @relationship.valid?
+  end
+
+  test "should require a followed" do
+    @relationship.followed = nil
+    assert_not @relationship.valid?
+  end
 end
