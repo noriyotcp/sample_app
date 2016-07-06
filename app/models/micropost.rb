1be23dcc896d02616b8d6499edcba78d2e200ce4
Noriyo Akita <noriyo.akita@gmail.com>
2016-07-06 00:32:35 +0000

    (micropost) Add mount_uploader

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :content, presence: true, length: { maximum: 140 }
 end
