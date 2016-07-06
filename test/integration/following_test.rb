8252846cc5484bfa4331e89c447cf22ea3c2b241
Noriyo Akita <noriyo.akita@gmail.com>
2016-07-06 15:40:20 +0000

    Fix rubocop

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -7,7 +7,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
   end
 
-  test "following page" do
+  test 'following page' do
     get following_user_path(@user)
     assert_not @user.following.empty?
     assert_match @user.following.count.to_s, response.body
@@ -16,7 +16,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "followers page" do
+  test 'followers page' do
     get followers_user_path(@user)
     assert_not @user.followers.empty?
     assert_match @user.followers.count.to_s, response.body
