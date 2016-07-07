2ea2e334edf1ec849d32e06932e624424ae4d02e
Noriyo Akita <noriyo.akita@gmail.com>
2016-07-07 08:10:56 +0000

    (models/micropost) Remove default_scope

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :content, presence: true, length: { maximum: 140 }
   validate :picture_size
